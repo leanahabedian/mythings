de444331092b05f0f0009649f194d64c90cf54e8
Leandro Ezequiel Nahabedian <lnahabedian@ws3.labo5>
2012-06-12 21:09:33 -0300

    prolog 3

--- prolog.pl
+++ prolog.pl
@@ -16,3 +16,14 @@ sacarPrimero([_|Xs], Xs).
 maxLista([X],X).
 maxLista([X,Y|Xs],Max) :-  X >= Y, Max is X, maxLista([X|Xs],Max).
 maxLista([X,Y|Xs],Max) :- X < Y, Max is Y,  maxLista([Y|Xs],Max).
+
+palindromo(List,R) :- reverso(List,RList), append(List, RList,R).
+
+doble([],[]).
+doble([X|List],R) :- doble(List,R1), append([X,X],R1,R).
+
+prefijo(P,Lista) :- append(P,_,Lista).
+
+sufijo(P,Lista) :- append(_,P,Lista).
+
+sublista(S,L) :- prefijo(S,L); sufijo(S,L).
